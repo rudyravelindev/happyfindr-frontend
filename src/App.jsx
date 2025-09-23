0847a1c0570e13cc34ff11e1e06bc33eb795c7f8
rudyravelindev <rudyravelindev@gmail.com>
2025-09-23 12:41:11 -0700

    Resolve merge conflict in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,11 @@ import CTA from './components/CTA.jsx';
 import Footer from './components/Footer.jsx';
 import Signup from './components/Signup.jsx';
 import Login from './components/Login.jsx';
-<<<<<<< HEAD
 import Dashboard from './components/Dashboard.jsx';
 import Items from './components/Items.jsx';
 import RegisterItem from './components/RegisterItem.jsx';
 import Profile from './components/Profile.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-=======
->>>>>>> 7750dd0 (creating the signup and login pages)
 
 function App() {
   return (
@@ -23,11 +20,7 @@ function App() {
       <Navbar />
       <main>
         <Routes>
-<<<<<<< HEAD
-          {/* Home Route */}
-=======
-          {' '}
->>>>>>> 7750dd0 (creating the signup and login pages)
+          {/* Home Route */}{' '}
           <Route
             path="/"
             element={
@@ -39,12 +32,9 @@ function App() {
               </>
             }
           />
-<<<<<<< HEAD
-
           {/* Auth Routes */}
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-
           {/* Dashboard Routes - Protected */}
           <Route
             path="/dashboard"
@@ -59,10 +49,8 @@ function App() {
             <Route path="register-item" element={<RegisterItem />} />
             <Route path="profile" element={<Profile />} />
           </Route>
-=======
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
->>>>>>> 7750dd0 (creating the signup and login pages)
         </Routes>
       </main>
       <Footer />
