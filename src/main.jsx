624aae067ad1c08596566986dec4c19df6fd229b
rudyravelindev <rudyravelindev@gmail.com>
2025-09-24 21:02:00 -0700

    adding the hasroute

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './components/AuthContext.jsx'; // Import AuthProvider
+import { HashRouter } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext.jsx';
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
